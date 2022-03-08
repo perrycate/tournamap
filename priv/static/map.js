9f6ee415a63611af3cab5adc1ca3f3a9928674ce
Perry Cate <NPCate01@gmail.com>
2022-03-07 18:56:20 -0800

    Reduce max zoom level to 13.
    This conserves on tile loads if somebody is just scrolling around.
    Zooming in is pretty expensive, and we don't actually need to get that
    close, I don't think.

--- priv/static/map.js
+++ priv/static/map.js
@@ -1,7 +1,7 @@
 "use strict";
 let myMap = L.map("mapid").setView([51.505, -0.09], 4);
 let OpenStreetMap_Mapnik = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ", {
-    maxZoom: 19,
+    maxZoom: 13,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
