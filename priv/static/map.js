e37b27a64771cf64e7200c9a0aff98dffb60c8c7
Perry Cate <NPCate01@gmail.com>
2022-04-15 18:54:12 -0700

    Add "About" section.
    Not terribly pretty, but it has the relevant info.

--- priv/static/map.js
+++ priv/static/map.js
@@ -1,5 +1,5 @@
 "use strict";
-let myMap = L.map("mapid", {
+let myMap = L.map("map", {
     worldCopyJump: true,
 }).setView([51.505, -0.09], 4);
 let OpenStreetMap_Mapnik = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ", {
