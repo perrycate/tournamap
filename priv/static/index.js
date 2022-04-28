287f4d6d5c7e8d5c3b2ce6e2389af3f49cfae177
Perry Cate <NPCate01@gmail.com>
2022-04-27 18:58:36 -0700

    Tidy map.js.
    Did multiple things but I don't feel like splitting my commits, whoops.
    1. Fixed null reference from using the wrong element ID.
    2. Fixed a couple obsolete log messages.
    3. Prettier touched a bunch of ugly formatting and I don't feel like
       fighting it.

--- priv/static/index.js
+++ priv/static/index.js
@@ -2,7 +2,6 @@
 // Contains code for managing the index.html page itself.
 // Does not contain the logic for managing the map and tournament data,
 // that can be found in map.js.
-console.log("file loaded");
 
 // Toggle about section when the nav button is clicked.
 document.getElementById("about-btn").addEventListener("click", function (e) {
--- priv/static/map.js
+++ priv/static/map.js
@@ -1,23 +1,29 @@
 "use strict";
-let myMap = L.map("map", {
-    worldCopyJump: true,
+const MAP_CONTAINER_ID = "map";
+
+let myMap = L.map(MAP_CONTAINER_ID, {
+  worldCopyJump: true,
 }).setView([51.505, -0.09], 4);
-let OpenStreetMap_Mapnik = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ", {
+let OpenStreetMap_Mapnik = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ",
+  {
     maxZoom: 14,
     minZoom: 3,
     maxNativeZoom: 11,
-    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-}).addTo(myMap);
+    attribution:
+      '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+  }
+).addTo(myMap);
 
 // Fetch tournament data and add pins to the map for each tournament.
 fetch("tournaments.json").then((resp) => {
-    resp.json().then((parsed_tourneys) => {
-        let tourney_markers = parsed_tourneys.map((tourney) => {
-            let { lat, lng } = tourney["location"];
-            return L.marker().setLatLng([lat, lng]).bindPopup(popupFor(tourney));
-        });
-        L.layerGroup(tourney_markers).addTo(myMap);
-    })
+  resp.json().then((parsed_tourneys) => {
+    let tourney_markers = parsed_tourneys.map((tourney) => {
+      let { lat, lng } = tourney["location"];
+      return L.marker().setLatLng([lat, lng]).bindPopup(popupFor(tourney));
+    });
+    L.layerGroup(tourney_markers).addTo(myMap);
+  });
 });
 
 // Sets loader and overlay while user responds to location prompt.
@@ -26,37 +32,35 @@ let loader = document.createElement("div");
 overlay.className = "overlay";
 loader.className = "loader";
 
-let mapContainer = document.getElementById("mapid");
-console.log(mapContainer);
+let mapContainer = document.getElementById(MAP_CONTAINER_ID);
 mapContainer.appendChild(overlay);
 mapContainer.appendChild(loader);
 
 // Set position based on user location, if they grant it.
 // Removes overlay and loader during success or failure.
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(setPosition, showError)
-}
-else {
-    alert("Please enable the browser to know your location or Geolocation is not supported by this browser.");
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  alert("Please enable the browser to know your location or Geolocation is not supported by this browser.");
 }
 
 function setPosition(resp) {
-    myMap.setView([resp.coords.latitude, resp.coords.longitude], 10);
-    mapContainer.removeChild(overlay);
-    mapContainer.removeChild(loader);
+  myMap.setView([resp.coords.latitude, resp.coords.longitude], 10);
+  mapContainer.removeChild(overlay);
+  mapContainer.removeChild(loader);
 }
 
 function showError() {
-    console.log("Location denied.");
-    mapContainer.removeChild(overlay);
-    mapContainer.removeChild(loader);
+  console.log("Location denied.");
+  mapContainer.removeChild(overlay);
+  mapContainer.removeChild(loader);
 }
 
 // Returns a string containing the html code that should be embedded in a leaflet
 // map popup for a particular tourney.
 function popupFor(tourney) {
-    return `
+  return `
 <h2><a href=${tourney["url"]} target="_blank">${tourney["name"]}</a></h2>
 <p>${new Date(tourney["start_time"] * 1000).toLocaleString()}</p>
-`
+`;
 }
