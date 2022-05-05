41691676273b42c00fb6a3e5132cbcc73e203386
Perry Cate <NPCate01@gmail.com>
2022-05-04 20:38:25 -0700

    Copy in files from create-react-app.
    Gonna combine this into one project, committing now juuust in case.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Don't tell me what to do.</h1>
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
