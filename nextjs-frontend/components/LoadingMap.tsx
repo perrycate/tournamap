b7f01337b3432d028caf7fec2426ca1d9a8487be
auoie <105099407+auoie@users.noreply.github.com>
2022-05-10 21:05:24 -0700

    make sure that the spinning circle doesn't change shape when the viewport width is small

--- nextjs-frontend/components/LoadingMap.tsx
+++ nextjs-frontend/components/LoadingMap.tsx
@@ -3,7 +3,7 @@ import { FC, memo } from "react";
 const LoadingMap: FC = () => {
   return (
     <div className="z-[1200] flex-1 flex bg-[rgba(0,0,0,0.5)] absolute left-0 right-0 top-0 bottom-0">
-      <div className="w-32 h-32 rounded-full border-neutral-100 border-t-blue-400 animate-spin border-[16px] m-auto"></div>
+      <div className="w-32 h-32 rounded-full border-neutral-100 animate-spin border-t-blue-400 border-[16px] m-auto flex-none"></div>
     </div>
   );
 };
