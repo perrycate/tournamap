5dd0c6dc7e30a6cf2c6e36ab1d74f5c1a1859c19
Perry Cate <NPCate01@gmail.com>
2022-08-29 21:43:06 -0700

    Refactor Map.js to make JS more modern looking.
    Shoutout to dota2attitude on twitch for the initial PR.
    Made some other changes on top of it to simplify the cache-handling
    logic.
    
    commit 75c198ff67dbee0c2a9b0f0e712fa63bed0105b4
    Author: Perry Cate <NPCate01@gmail.com>
    Date:   Mon Aug 29 21:38:26 2022 -0700
    
        Factor out all location/cache management stuff into a separate function.
    
        The main map logic now has no idea that the cache even exists.
    
    commit 6f758b80c6cefeaddc743fbafd18f039745e920c
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Mon Aug 29 23:48:56 2022 -0300
    
        fix last minute fixes
    
    commit bc8342117a9b132d819b46229d7eb8dd9de32766
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Mon Aug 29 23:23:24 2022 -0300
    
        fix: inline function
    
    commit cf1f8579c28690b11c55a887e97560ac1f7f285d
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Mon Aug 29 23:13:11 2022 -0300
    
        fix: fix recursive error
    
    commit 2644ddda3855d576726356accaabf07d8f674b86
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 03:17:02 2022 -0300
    
        refactor: use a hook to capture all the location logic
    
    commit ea6182304e8096a6ca5579dc28695c5ee813bf57
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 02:47:53 2022 -0300
    
        make behaviour the same
    
    commit 1b19f96b2e4dc44918c6cd07b793b59e8a68e53c
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 01:58:11 2022 -0300
    
        fix overlay
    
    commit 1dded6062701b1b88563d3b67d9eadb6d6256c9d
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 01:50:30 2022 -0300
    
        fix loading thing
    
    commit f7971d1b3519dc8bb269f3b5afb2c260d956f609
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 01:36:24 2022 -0300
    
        nuke idea folder
    
    commit cc125e12fa391f54c344206c04113c37058e7e55
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 01:26:28 2022 -0300
    
        fix callback
    
    commit 59e0583221d38f79a0a44bd8a9d1b26c8c3f6809
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 00:48:48 2022 -0300
    
        fix array mistake and incorrect parameters on callback
    
    commit a89ed9596ec73e1c6aada9a3b909c7fc4f4d3b2a
    Author: CodeItQuick <evanontario@hotmail.com>
    Date:   Thu Aug 25 00:03:59 2022 -0300
    
        feat: refactor to use ECMA script more modern style

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,15 @@
 "use strict";
 
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { LayerGroup } from "react-leaflet/LayerGroup";
-import { useMap } from "react-leaflet/hooks";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {LayerGroup} from "react-leaflet/LayerGroup";
+import {useMap} from "react-leaflet/hooks";
 import L from "leaflet";
 
 import icon from "leaflet/dist/images/marker-icon.png";
 import icon2x from "leaflet/dist/images/marker-icon-2x.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import { Icon } from "leaflet/src/layer/marker/Icon"
 
 // We need to override the icon locations due to shenanigans in react-leaflet.
 let defaultIcons = new L.Icon.Default();
@@ -18,139 +17,114 @@ defaultIcons.options.iconUrl = icon;
 defaultIcons.options.iconRetinaUrl = icon2x;
 defaultIcons.options.shadowUrl = iconShadow;
 
-const MAP_CONTAINER_ID = "map";
 const DEFAULT_ZOOM_LEVEL = 10;
 const LOCATION_CACHE_KEY = "userLatLng";
-// Location to initialize the map to if no user location can be found.
-const FALLBACK_INITIAL_LOCATION = [51.505, -0.09];
+const FALLBACK_INITIAL_LOCATION = [51.505, -0.09]; // London or something idk.
 
-// Check for a cached user location to use as the initial view.
-let initialLocation = FALLBACK_INITIAL_LOCATION;
-let locationCacheHit = false;
-try {
-  let cachedLocationStr = localStorage.getItem(LOCATION_CACHE_KEY);
-  if (cachedLocationStr == null) {
-    console.log("User location not set in local storage.");
-  } else {
-    // Make sure the cached data is in roughly the format we expect.
-    let [lat, lng] = JSON.parse(cachedLocationStr);
-    initialLocation = [lat, lng];
+// Returns the user's location.
+// May load from the browser's geolocaton API, or from a localstorage cache.
+//
+// NOTE:
+// If the cached location is substantially different from the user's actual
+// location, we DO NOT modify the map position, and only silently update
+// the cache for next time.
+// Because the browser's geolocation API can take on the order of seconds to
+// finish, the user may have already started scrolling on the map, and may not
+// want the location to suddenly jump.
+// One day it would be nice to have a popup explicitly asking if they want to
+// jump to the new location. Until then, we prefer not to interrupt whatever
+// the user was doing, and instead cache it for the next time the page is loaded.
+const getLocation = () => {
+    // Kick off process to fetch our location and update the cache.
+    // We may not need to return this if we have a cache entry, but
+    // we will always want to run it so we update the cache with the
+    // new location if it changed.
+    const slowLocationAPIPromise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((location) => {
+            // Cache location for future use.
+            console.log("Stored user location in local storage.")
+            location = [location.coords.latitude, location.coords.longitude];
+            localStorage.setItem(LOCATION_CACHE_KEY, JSON.stringify(location));
 
-    locationCacheHit = true; // Should be the last thing we do in case we get an exception.
-  }
-} catch (e) {
-  console.log("Error while attempting to parse cached location from local storage:");
-  console.log(e);
-  localStorage.clear();
-  console.log("Local storage cleared.");
-}
-
-function getCurrentPosition() {
-  return new Promise((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        // Success.
-        (resp) => {
-          resolve([resp.coords.latitude, resp.coords.longitude]);
-        },
-        // Failure.
-        (e) => {
-          reject(e);
-        }
-      );
-    } else {
-      alert("Please enable the browser to know your location or Geolocation is not supported by this browser.");
-      reject();
-    }
-  });
-}
-
-function Map() {
-  const [tourneyData, setTourneyData] = useState([]);
-  const [mapCenter, setMapCenter] = useState(initialLocation);
-  // Until this is false, we wish to convey to the user that the view may suddenly
-  // change (for example, when the user's location is finished loading from the browser.)
-  // If we hit the location cache when this module was first loading, the view is probably
-  // close to where the user actually is, and thus should not
-  // suddenly change.
-  const [locationLoading, setLocationLoading] = useState(!locationCacheHit);
-
-  // Load tournament data once.
-  useEffect(() => {
-    fetch("tournaments.json").then((resp) => {
-      resp.json().then(setTourneyData);
+            resolve(location);
+        }, (err) => {
+            console.log(err);
+            reject(err);
+        });
     });
-  }, []); // Passing in empty array means this only runs once.
 
-  // Attempt to get location from the browser once.
-  useEffect(() => {
-    getCurrentPosition()
-      .then((location) => {
-        // Cache location for future use.
-        localStorage.setItem(LOCATION_CACHE_KEY, JSON.stringify(location));
+    // Return location from cache immedately if it is available.
+    const cachedLocationStr = localStorage.getItem(LOCATION_CACHE_KEY);
+    if (cachedLocationStr != null) {
+        return Promise.resolve(JSON.parse(cachedLocationStr))
+    }
 
-        // Only set the view if it hasn't been set yet.
-        // We don't want to interfere with any scrolling
-        // the user did while we were searching for the location.
-        if (locationLoading) {
-          console.log("updating location");
-          setMapCenter(location);
-        }
-      })
-      .catch((e) => console.log(e))
-      .finally(() => {
-        // Remove loader overlay, if applicable.
-        setLocationLoading(false);
-      });
-  }, []);
+    // Otherwse, wait for the slower process.
+    console.log("User location not set in local storage.");
+    return slowLocationAPIPromise;
+}
 
-  return <>
-    <a href="http://mapbox.com/about/maps" className="mapbox-logo" target="_blank">Mapbox</a>
-    <MapContainer center={mapCenter} zoom={DEFAULT_ZOOM_LEVEL} id="map" worldCopyJump={true}>
-      {locationLoading && <Overlay />}
-      <ViewChanger center={mapCenter} />
-      <TileLayer
-        attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-        url="https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ"
-        maxZoom={14}
-        minZoom={3}
-        maxNativeZoom={11}
-      >
-      </TileLayer>
-      <LayerGroup>
-        {
-          tourneyData.map((tourneyJSON) =>
-            <Marker key={tourneyJSON.url} position={tourneyJSON.location} icon={defaultIcons}>
-              <Popup>
-                <h2>
-                  <a href={tourneyJSON["url"]} target="_blank">{tourneyJSON["name"]}</a>
-                </h2>
-                <p>{new Date(tourneyJSON["start_time"] * 1000).toLocaleString()}</p>
-              </Popup>
-            </Marker>
-          )
-        }
-      </LayerGroup>
-    </MapContainer>
-  </>
+const Map = () => {
+    const [tourneyData, setTourneyData] = useState([]);
+    const [mapCenter, setMapCenter] = useState(FALLBACK_INITIAL_LOCATION);
+    const [locationLoading, setLocationLoading] = useState(true);
+
+    // Fetch location on first render.
+    useEffect(() => {
+        const userLocation = getLocation().then((location) => {
+            setMapCenter(location);
+            setLocationLoading(false);
+        });
+    }, []);
+
+    // Load tournament data on first render.
+    useEffect(() => {
+        fetch("tournaments.json")
+            .then(resp => resp.json().then(setTourneyData));
+    }, []);
+
+    return <>
+        <a href="http://mapbox.com/about/maps" className="mapbox-logo" target="_blank">Mapbox</a>
+        <MapContainer center={mapCenter} zoom={DEFAULT_ZOOM_LEVEL} id="map" worldCopyJump={true}>
+            {locationLoading && <>
+                <div className="overlay"></div>
+                <div className="loader"></div>
+            </>}
+            <ViewChanger center={mapCenter}/>
+            <TileLayer
+                attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+                url="https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JhdmlkZGQiLCJhIjoiY2wwZDh3eDE2MDZ1OTNrcGYybjhsNmN2diJ9.cPvRZK6WTt_wjQSa-DzblQ"
+                maxZoom={14}
+                minZoom={3}
+                maxNativeZoom={11}
+            >
+            </TileLayer>
+            <LayerGroup>
+                {
+                    tourneyData.map((tourneyJSON) =>
+                        <Marker key={tourneyJSON.url} position={tourneyJSON.location} icon={defaultIcons}>
+                            <Popup>
+                                <h2>
+                                    <a href={tourneyJSON["url"]} target="_blank">{tourneyJSON["name"]}</a>
+                                </h2>
+                                <p>{new Date(tourneyJSON["start_time"] * 1000).toLocaleString()}</p>
+                            </Popup>
+                        </Marker>
+                    )
+                }
+            </LayerGroup>
+        </MapContainer>
+    </>
 }
 
 // The view location (center, zoom) as set in the MapContainer properties only
 // affects the INITIAL view location. Anything after the fact must be set using
 // the useMap() hook, which is only available to children of MapContainer.
 // It's wonky but whatever.
-function ViewChanger({ center }) {
-  let map = useMap()
-  map.setView(center, map.zoom);
-  return null;
-}
-
-// Grayed-out overlay with a circular loader thingy in the middle.
-function Overlay() {
-  return <>
-    <div className="overlay"></div>
-    <div className="loader"> </div>
-  </>
+const ViewChanger = ({center}) => {
+    const map = useMap() // prefer const over let
+    map.setView(center, map.zoom);
+    return null;
 }
 
 export default Map;
