0977894c0a6d08bb4976a1698893891c6d57b6fc
Perry Cate <ncate@alumni.princeton.edu>
2022-11-30 20:53:57 -0800

    Compress image in build/load scripts

--- deploy/build_image.sh
+++ deploy/build_image.sh
@@ -3,4 +3,4 @@
 # This tar file can then be copied to a server somewhere, for example after
 # making fetcher code changes.
 docker build -t tournamap-download .
-docker save tournamap-download > tournamap-download.tar
+docker save tournamap-download | bzip2 > tournamap-download.tar.bz2
--- deploy/load_image.sh
+++ deploy/load_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Load the docker image for our fetcher script from a compressed file.
 
-# Docker load apparently supports gzipped files too.
-docker load < tournamap-download.tar.gz 
+# Docker load apparently supports compressed files too.
+docker load < tournamap-download.tar.bz2
