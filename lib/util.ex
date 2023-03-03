d6965c88b5311a3a40e283206aea81bb16dfe556
Wellington Anthony Johnson II <50687671+ajistrying@users.noreply.github.com>
2023-03-03 02:45:58 -0500

    Cleaner error handling within elixir util file (#57)
    * do a bit of refactoring and add in some error handling to the util elixir file
    
    * more tweaks
    
    * clean up try-dos and use more idiomatic pattern matching to catch error cases
    
    * there was no success true t match on, so match on the assumed successfully returned data
    
    * Ensure we use System.stop, make sure to print to stderr, refactor handling of decoded JSON response logic
    
    * move nodes up as well
    
    * address latest suggestions

--- lib/util.ex
+++ lib/util.ex
@@ -2,11 +2,17 @@ defmodule Util do
   @auth_env_var "SMASH_GG_TOKEN"
   @default_storage_file "tournaments.json"
 
-  def update_tourneys(filename \\ @default_storage_file) do
-    {:ok, tourneys} = get_all_tourneys()
-    tourneys
-    |> JSON.encode!
-    |> then(&File.write!(filename, &1))
+  def update_tourneys do
+    case get_all_tourneys() do
+      {:ok, tourneys} ->
+        tourneys
+        |> JSON.encode!
+        |> then(&File.write!(@default_storage_file, &1))
+      {:error, reason} ->
+        IO.puts(:stderr, reason)
+        System.stop(1)
+    end
+
   end
 
   def get_all_tourneys do
@@ -17,45 +23,45 @@ defmodule Util do
         # let's just assume there are no more than 10 lol.
         # (At time of writing there are less than 1,000 upcoming tournaments,
         # which can easily fit in only 2 pages.)
-        tourneys = 1..10
-                   |> Enum.map(&get_tourney_page(auth, &1))
-                   |> List.flatten
-        { :ok, %{ tournament_data: tourneys, metadata: %{ updated_at: DateTime.now!("Etc/UTC") |> DateTime.to_unix()}} }
+        tourneys =
+          1..10
+          |> Enum.map(&get_tourney_page(auth, &1))
+          |> List.flatten
+
+          { :ok, %{ tournament_data: tourneys, metadata: %{ updated_at: DateTime.now!("Etc/UTC") |> DateTime.to_unix()}} }
     end
   end
 
   def get_tourney_page(auth, page_num) do
     body = %{
       "query" => "query AllUltimateTournaments($perPage: Int, $pageNum: Int) {
-  tournaments(query: {
-    page: $pageNum,
-    perPage: $perPage
-    filter: {
-      videogameIds: [1386]
-      upcoming: true
-      hasOnlineEvents: false
-    }
-  }) {
-    pageInfo {
-      total
-      totalPages
-      page
-      perPage
-      sortBy
-      filter
-    }
-    nodes {
-      id
-      name
-      lat
-      lng
-      startAt
-      endAt
-      updatedAt
-      slug
-    }
-  }
-}",
+        tournaments(query: {
+          page: $pageNum,
+          perPage: $perPage
+          filter: {
+            videogameIds: [1386]
+            upcoming: true
+            hasOnlineEvents: false
+          }
+        }) { pageInfo {
+            total
+            totalPages
+            page
+            perPage
+            sortBy
+            filter
+          } nodes {
+            id
+            name
+            lat
+            lng
+            startAt
+            endAt
+            updatedAt
+            slug
+          }
+        }
+      }",
       "variables" => %{
         "perPage" => 500,
         "pageNum" => page_num
@@ -67,10 +73,24 @@ defmodule Util do
       Authorization: "Bearer " <> auth
     ]
 
-    {:ok, resp} = HTTPoison.post("https://api.start.gg/gql/alpha", JSON.encode!(body), headers)
+    case HTTPoison.post("https://api.start.gg/gql/alpha", JSON.encode!(body), headers) do
+      {:ok, resp} ->
+        resp.body
+        |> JSON.decode
+        |> format_decoded_response
+      {:error, reason} ->
+        IO.puts(:stderr, reason)
+        System.stop(1)
+    end
+  end
 
-    resp.body
-    |> JSON.decode!()
+  def format_decoded_response({:ok, decoded_map = %{"success" => false}}) do
+    IO.puts(:stderr, Map.get(decoded_map, "message"))
+    System.stop(1)
+  end
+
+  def format_decoded_response({:ok, decoded_map}) do
+    decoded_map
     |> Map.get("data")
     |> Map.get("tournaments")
     |> Map.get("nodes")
@@ -88,6 +108,6 @@ defmodule Util do
         "updated_at" => sgg_data["updatedAt"],
         "url" => "https://start.gg/" <> (sgg_data["slug"])
       }
-    end)
+      end )
   end
 end
